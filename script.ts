313e271301c22a3f7a35692c2a08ee09495760c1
kazi983 <kaoru.kajihara.mail@gmail.com>
2026-08-17 11:01:20 -0700

    complete step1: Deposit class

--- script.ts
+++ script.ts
@@ -1,38 +1,47 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 let balance = 500.0;
+
 class Withdrawal {
-    amount;
-    constructor(amount) {
-        this.amount = amount;
-    }
-    commit() {
-        balance -= this.amount;
-    }
+  private amount: number;
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance -= this.amount;
+  }
 }
+
 class Deposit {
-    amount;
-    constructor(amount) {
-        this.amount = amount;
-    }
-    commit() {
-        balance += this.amount;
-    }
+  private amount: number;
+  constructor(amount: number) {
+    this.amount = amount;
+  }
+
+  commit() {
+    balance += this.amount;
+  }
 }
+
 /**
  * Test code for Step 0
  */
+
 const t1 = new Withdrawal(50.25);
 t1.commit();
 console.log('Transaction 1:', t1);
+
 const t2 = new Withdrawal(9.99);
 t2.commit();
 console.log('Transaction 2:', t2);
+
 console.log('Balance:', balance);
+
 /**
  * Test code for Step 1
  */
+
 const t3 = new Deposit(120.0);
 t3.commit();
 console.log('Transaction 3:', t3);
+
 console.log('Balance:', balance);
