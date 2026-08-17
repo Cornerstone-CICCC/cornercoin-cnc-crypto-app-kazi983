3cf93e317288eb5421791ffa406b6bbf0b291c16
kazi983 <kaoru.kajihara.mail@gmail.com>
2026-08-17 11:23:33 -0700

    complete step3: Refactor deposit and withdrawal classes

--- script.js
+++ script.js
@@ -1,50 +1,58 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-let balance = 500.0;
 class Account {
     userName;
-    balance;
+    _balance = 500.0;
     constructor(userName) {
         this.userName = userName;
     }
+    get balance() {
+        return this._balance;
+    }
+    withdrawal(amount) {
+        this._balance -= amount;
+    }
+    deposit(amount) {
+        this._balance += amount;
+    }
 }
 class Withdrawal {
+    account;
     amount;
-    constructor(amount) {
+    constructor(account, amount) {
+        this.account = account;
         this.amount = amount;
     }
     commit() {
-        balance -= this.amount;
+        this.account.withdrawal(this.amount);
     }
 }
 class Deposit {
+    account;
     amount;
-    constructor(amount) {
+    constructor(account, amount) {
+        this.account = account;
         this.amount = amount;
     }
     commit() {
-        balance += this.amount;
+        this.account.deposit(this.amount);
     }
 }
-/**
- * Test code for Step 0
- */
-const t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-const t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-console.log('Balance:', balance);
-/**
- * Test code for Step 1
- */
-const t3 = new Deposit(120.0);
-t3.commit();
-console.log('Transaction 3:', t3);
-console.log('Balance:', balance);
-/**
- * Test code for Step 2
- */
+/*************************************
+ *
+ *  Driver
+ *
+ *************************************/
 const myAccount = new Account('snow-patrol');
-console.log('myAccount:', myAccount);
+const t1 = new Withdrawal(myAccount, 50.25);
+t1.commit();
+// console.log('Transaction 1:', t1);
+console.log('Balance:', myAccount.balance, `(correct output: ${500 - 50.25})`);
+const t2 = new Withdrawal(myAccount, 9.99);
+t2.commit();
+// console.log('Transaction 2:', t2);
+console.log('Balance:', myAccount.balance, `(correct output: ${500 - 50.25 - 9.99})`);
+const t3 = new Deposit(myAccount, 120.0);
+t3.commit();
+// console.log('Transaction 3:', t3);
+console.log('Balance:', myAccount.balance, `(correct output: ${500 - 50.25 - 9.99 + 120})`);
--- script.ts
+++ script.ts
@@ -1,63 +1,81 @@
-let balance = 500.0;
-
 class Account {
   private userName: string;
-  private balance?: number;
+  protected _balance: number = 500.0;
 
   constructor(userName: string) {
     this.userName = userName;
   }
+
+  get balance() {
+    return this._balance;
+  }
+
+  withdrawal(amount: number) {
+    this._balance -= amount;
+  }
+
+  deposit(amount: number) {
+    this._balance += amount;
+  }
 }
 
 class Withdrawal {
+  private account: Account;
   private amount: number;
-  constructor(amount: number) {
+
+  constructor(account: Account, amount: number) {
+    this.account = account;
     this.amount = amount;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.withdrawal(this.amount);
   }
 }
 
 class Deposit {
+  private account: Account;
   private amount: number;
-  constructor(amount: number) {
+
+  constructor(account: Account, amount: number) {
+    this.account = account;
     this.amount = amount;
   }
 
   commit() {
-    balance += this.amount;
+    this.account.deposit(this.amount);
   }
 }
 
-/**
- * Test code for Step 0
- */
-
-const t1 = new Withdrawal(50.25);
-t1.commit();
-console.log('Transaction 1:', t1);
-
-const t2 = new Withdrawal(9.99);
-t2.commit();
-console.log('Transaction 2:', t2);
-
-console.log('Balance:', balance);
-
-/**
- * Test code for Step 1
- */
-
-const t3 = new Deposit(120.0);
-t3.commit();
-console.log('Transaction 3:', t3);
-
-console.log('Balance:', balance);
-
-/**
- * Test code for Step 2
- */
-
+/*************************************
+ *
+ *  Driver
+ *
+ *************************************/
 const myAccount = new Account('snow-patrol');
-console.log('myAccount:', myAccount);
+
+const t1 = new Withdrawal(myAccount, 50.25);
+t1.commit();
+// console.log('Transaction 1:', t1);
+
+console.log('Balance:', myAccount.balance, `(correct output: ${500 - 50.25})`);
+
+const t2 = new Withdrawal(myAccount, 9.99);
+t2.commit();
+// console.log('Transaction 2:', t2);
+
+console.log(
+  'Balance:',
+  myAccount.balance,
+  `(correct output: ${500 - 50.25 - 9.99})`,
+);
+
+const t3 = new Deposit(myAccount, 120.0);
+t3.commit();
+// console.log('Transaction 3:', t3);
+
+console.log(
+  'Balance:',
+  myAccount.balance,
+  `(correct output: ${500 - 50.25 - 9.99 + 120})`,
+);
